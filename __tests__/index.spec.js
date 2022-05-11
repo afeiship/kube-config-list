0038970d8e1bde6872e04713e98881c152bee7b8
afeiship <1290657123@qq.com>
2022-05-11 09:01:04 +0800

    wip

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 test('change ', () => {
   // todo: how to test this cli program?
   const res = require('../bin');
-  process.stdout.write(' -b');
-  // process.stdin.emit('keypress', '', { name: 'enter' });
+  process.stdin.emit('keypress', '', { name: 'enter' });
   // process.stdin.emit('keypress', '', { name: 'enter' });
   // process.stdin.on('readable', function () {
   //   var chunk = process.stdin.read(); // 获取到输入的信息
