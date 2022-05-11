92ace4c0df9237c50e7117d6f36b0472e517a8eb
afeiship <1290657123@qq.com>
2022-05-11 09:39:00 +0800

    test: add boot -b args unit testing

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
+const { exec } = require('child_process');
 
-test('change ', () => {
-  // todo: how to test this cli program?
-  const res = require('../bin');
-  process.stdin.emit('keypress', '', { name: 'enter' });
-  // process.stdin.emit('keypress', '', { name: 'enter' });
-  // process.stdin.on('readable', function () {
-  //   var chunk = process.stdin.read(); // 获取到输入的信息
-  //   console.log('chunk:', chunk);
-  //   process.stdin.emit('end'); // 触发end事件
-  // });
-  // ks.sendKeys([' ','-', 'b', 'enter']);
-  // console.warn('res:', console.log.mock.calls);
-  // console.log(process.env);
-  process.stdin.emit('end');
+describe('boot.sh exists/not exists', () => {
+  test('boot should be not override when boot.sh exists.', (done) => {
+    exec(`node ./bin/index.js -b`, (err, stdout, stderr) => {
+      expect(stdout.includes('/.kube/boot.sh existes, ignore.')).toBe(true);
+      done();
+    });
+  });
+
+  test('boot should be created when boot.sh not exists', (done) => {
+    const homedir = require('os').homedir();
+    const kube_boot = `${homedir}/.kube/boot.sh`;
+    // delete when exist
+    if (fs.existsSync(kube_boot)) fs.rmSync(kube_boot);
+
+    expect(fs.existsSync(kube_boot)).toBe(false);
+
+    exec(`node ./bin/index.js -b`, (err, stdout, stderr) => {
+      console.log(stdout);
+      expect(fs.existsSync(kube_boot)).toBe(true);
+      done();
+    });
+  });
 });
